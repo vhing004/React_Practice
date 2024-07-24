a25686ec88d4ce2a5b442307d4eac7f5ff96ccb6
vhing004 <hvinh12112004@gmail.com>
2024-07-25 05:18:44 +0700

    fix load button data

--- src/components/loadMoreData/index.jsx
+++ src/components/loadMoreData/index.jsx
@@ -10,12 +10,13 @@ function LoadMoreData() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
 
+  // function
   const fetchApi = async () => {
     try {
       setLoading(true);
 
       const reponse = await fetch(
-        `https://dummyjson.com/products?limit=10&skip=${
+        `https://dummyjson.com/products?limit=5&skip=${
           count === 0 ? 0 : count * 10
         }`
       );
@@ -23,22 +24,32 @@ function LoadMoreData() {
 
       if (data && data.products) {
         setLoading(false);
-        // console.log(data);
-        // setProducts(data.products);
-        setProducts((prevData) => [...prevData, ...data.products]);
+        console.log(data, "data");
+        // setProduct(data.products);
+        // setProduct((prevData) => [...prevData, ...data.products]);
+        setProducts((prevData) => {
+          const newProducts = [...prevData, ...data.products];
+          // nếu có phần tử trùng id thì chỉ giữ 1 phần tử lại.
+          const uniqueProducts = newProducts.filter(
+            (product, index, self) =>
+              index === self.findIndex((p) => p.id === product.id)
+          );
+          return uniqueProducts;
+        });
       }
-      //   console.log(data.products);
     } catch (e) {
       setLoading(true);
       console.log(e);
     }
   };
 
+  // gọi hàm
   useEffect(() => {
     fetchApi();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [count]);
 
+  // set trạng thái disable
   useEffect(() => {
     if (products.length === 50) {
       setDisable(true);
